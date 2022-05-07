2cba99e110c91c430da3b7f50348c75bffbbf32c
Amaan Ali <amaanali2002@hotmail.com>
2022-05-07 00:22:32 +0000

    created standard text element

---
+++ components/texts/Text.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+import { useFonts, Rubik_500Medium } from '@expo-google-fonts/rubik';
+
+export default function Text(props) {
+    let [fontsLoaded] = useFonts({
+        Rubik_500Medium,
+    });
+
+    if (!fontsLoaded) {
+        return null;
+    }
+
+    return <T style={{ fontFamily: 'Rubik_500Medium' }}>{props.children}</T>;
+}
+
+const T = styled.Text`
+    font-weight: 500;
+    font-size: 17px;
+    color: #9ca3af;
+    text-align: center;
+    padding-vertical: 4px;
+    line-height: 24px;
+`;
