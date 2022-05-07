dcb76529efc1dd013728b2c3df7a449c4e9b6cfa
Amaan Ali <amaanali2002@hotmail.com>
2022-05-07 04:03:57 +0000

    status bar

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+import { StatusBar } from 'expo-status-bar';
 import { NavigationContainer } from '@react-navigation/native';
 import TabLayout from './navigation/TabLayout.js';
 
@@ -5,6 +6,7 @@ export default function App() {
     return (
         <NavigationContainer>
             <TabLayout />
+            <StatusBar style='auto' />
         </NavigationContainer>
     );
 }
