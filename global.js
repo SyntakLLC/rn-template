c06b58cbacac5f4fe4a107cb6441127f9e32bfc8
Amaan Ali <amaanali2002@hotmail.com>
2022-05-07 17:26:59 +0000

    added border color

--- global.js
+++ global.js
@@ -5,10 +5,12 @@ global.primaryColor = '#007aff';
 // light mode
 global.textColor = '#0c4a6e';
 global.bgColor = '#fff';
+global.borderColor = '#00000030';
 
 // if dark mode
 const colorScheme = Appearance.getColorScheme();
 if (colorScheme === 'dark') {
     global.textColor = '#cffafe';
     global.bgColor = '#181a1b';
+    global.borderColor = '#ffffff30';
 }
