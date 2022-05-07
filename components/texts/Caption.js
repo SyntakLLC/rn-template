ca77a541ea956cca55c95a13cd510b8b180f6e94
Amaan Ali <amaanali2002@hotmail.com>
2022-05-07 00:22:12 +0000

    created smallest level of text

---
+++ components/texts/Caption.js
@@ -0,0 +1,32 @@
+import styled from 'styled-components';
+import { useFonts, Rubik_500Medium } from '@expo-google-fonts/rubik';
+
+export default function Caption({ children, uppercase = false }) {
+    let [fontsLoaded] = useFonts({
+        Rubik_500Medium,
+    });
+
+    if (!fontsLoaded) {
+        return null;
+    }
+
+    return (
+        <T
+            style={{
+                fontFamily: 'Rubik_500Medium',
+                fontWeight: uppercase ? '700' : '600',
+                textTransform: uppercase ? 'uppercase' : 'none',
+            }}
+        >
+            {children}
+        </T>
+    );
+}
+
+const T = styled.Text`
+    font-weight: 600;
+    font-size: 14px;
+    color: gray;
+    text-align: center;
+    padding-vertical: 4px;
+`;
