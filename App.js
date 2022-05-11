d2803e0b21143f3861fc39f6f201973e8bb894ad
Amaan Ali <amaanali2002@hotmail.com>
2022-05-11 18:53:42 +0000

    uses global colorScheme prop for status bar now

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { NavigationContainer } from '@react-navigation/native';
 import TabLayout from './navigation/TabLayout.js';
+import './global.js';
 
 export default function App() {
     return (
         <NavigationContainer>
             <TabLayout />
-            <StatusBar style='auto' />
+            <StatusBar style={global.colorScheme} />
         </NavigationContainer>
     );
 }
