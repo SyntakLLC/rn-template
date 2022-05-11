1315f2c65fde194d67ab4ea0b3883533097336f7
Amaan Ali <amaanali2002@hotmail.com>
2022-05-11 18:52:45 +0000

    added color scheme global var, edited dark color

--- global.js
+++ global.js
@@ -1,6 +1,7 @@
 import { Appearance } from 'react-native';
 
 global.primaryColor = '#007aff';
+global.colorScheme = Appearance.getColorScheme();
 
 // light mode
 global.textColor = '#0c4a6e';
@@ -8,9 +9,8 @@ global.bgColor = '#fff';
 global.borderColor = '#00000030';
 
 // if dark mode
-const colorScheme = Appearance.getColorScheme();
-if (colorScheme === 'dark') {
-    global.textColor = '#cffafe';
-    global.bgColor = '#181a1b';
+if (global.colorScheme !== 'dark') {
+    global.textColor = '#ffffff';
+    global.bgColor = '#111827';
     global.borderColor = '#ffffff30';
 }
